8abd270c1c625674b21b8e9434cf31b460c63b63
Roger Martins <rogermartinsdesign@gmail.com>
2023-12-14 23:59:03 -0300

    initial commit nextjs starter kit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="mt-4 text-3xl font-bold">Next JS Starter Kit</h1>
+      </div>
+    </section>
+  )
+}
